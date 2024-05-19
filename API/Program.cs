39c8c3180a6e6d38447fe4d9605f12c4e47a0728
= <=>
2024-05-19 10:14:14 -0700

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,19 @@
+using API.Services;
 using API.Data;
+using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => 
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipelinedsa.
@@ -21,6 +23,10 @@ var app = builder.Build();
 // app.UseAuthorization();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
